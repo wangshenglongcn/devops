ff0c66d190f9575b9f9fcb8ac943a4027cd8d2d4
muzhaoyang <xx>
2025-07-31 08:50:52 +0800

    init

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('posts/', views.post_list, name='post_list'),              # GET -- list posts, POST -- create new post
-    path('posts/<int:pk>/', views.post_detail, name='post_detail')  # GET -- post detail, POST -- edit post, DELETE -- delete post
-]
