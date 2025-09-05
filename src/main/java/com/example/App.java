7c6d3637bb698f2a2cf1554d8aea1e36c17316e6
muzhaoyang <muzhaoyang2022@outlook.com>
2025-09-05 22:28:29 +0800

    add a simple java project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+    public static void main(String[] args) {
+        log.info("Hello Bazel + Bzlmod + Maven deps!");
+    }
+}
+
